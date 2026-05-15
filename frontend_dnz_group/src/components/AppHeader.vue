0f6769d223dc32f19e0c19a22aff50359caa0f81
Tchinde Fosso <tchindefossomael@yahoo.fr>
2026-05-15 22:58:13 +0200

    teste

--- frontend_dnz_group/src/components/AppHeader.vue
+++ frontend_dnz_group/src/components/AppHeader.vue
@@ -11,12 +11,12 @@
           <ul v-show="servicesOpen" class="dropdown-menu" @click.stop>
             <li>
               <router-link to="/services/transport-voiture" @click="servicesOpen = false">
-                🚗 Transport Voitures1
+                🚗 Transport Voitures1fghtfdh
               </router-link>
             </li>
             <li>
               <router-link to="/services/transport-coli" @click="servicesOpen = false">
-                📦 Transport Colis
+                📦 Transport Colishlkö
               </router-link>
             </li>
           </ul>
